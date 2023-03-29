7e1ae534539f86300df3143379434860951ae25f
Milena Emmert <milena.emmert@outlook.com>
2023-03-28 22:09:14 -0300

    Sprint 2: route to main screen and hourly forecast

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { MAIN_ROUTE, HOURLY_ROUTE } from './contants/routes'
 import MainScreen from './screens/MainScreen'
+import HourlyForecastScreen from './screens/HourlyForecast/HourlyForecastScreen'
 import './assets/base/global.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
 root.render(
-  <MainScreen />
-);
+  <BrowserRouter>
+    <Routes>
+      <Route path={MAIN_ROUTE} element={<MainScreen />} />
+      <Route path={HOURLY_ROUTE} element={<HourlyForecastScreen />} />
+    </Routes>
+  </BrowserRouter>
+)
