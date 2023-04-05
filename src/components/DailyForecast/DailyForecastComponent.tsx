0e05bdd9af14cf483f1973ce2199349cda74f8c6
Milena Emmert <milena.emmert@outlook.com>
2023-04-05 00:52:25 -0300

    Free weather api with axios

--- src/components/DailyForecast/DailyForecastComponent.tsx
+++ src/components/DailyForecast/DailyForecastComponent.tsx
@@ -1,8 +1,11 @@
 // @ts-nocheck
-import forecast from '../../api/forecast.json'
+//import forecast from '../../api/forecast.json'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import s from './DailyForecast.module.css'
+import { render } from '@testing-library/react'
 
-const DAILY = {
+/* const DAILY = {
   ICON: { ALT: 'Icon representing the current weather condition.', REFERENCE: forecast.current.condition.icon },
   ITEMS: [
     { LABEL: 'Location', REFERENCE: forecast.location.name },
@@ -14,18 +17,40 @@ const DAILY = {
     { LABEL: 'Pressure', REFERENCE: forecast.current.pressure_mb },
     { LABEL: 'Last updated', REFERENCE: forecast.current.last_updated }
   ]
-}
+} */
 
 export default function DailyForecastComponent() {
+  const key = process.env.REACT_APP_API_KEY
+  const [weatherData, setWeatherData] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(
+        `https://api.weatherapi.com/v1/current.json?key=${key}&q=London&aqi=no`
+      )
+
+      console.log(data)
+      setWeatherData(data)
+    }
+    fetchData()
+  }, [])
+
+  function renderList() {
+    if(weatherData) {
+      return (
+        <ul className={s.itemsContainer}>
+          <li>{weatherData.location.country}</li>
+          <li>{weatherData.current.condition.text}</li>
+        </ul>
+      )
+    }
+  }
+
   return (
     <section className={s.section}>
-      <img src={DAILY.ICON.REFERENCE} alt={DAILY.ICON.ALT} />
-
-      <ul className={s.itemsContainer}>
-        {DAILY.ITEMS.map((item) => {
-          return <li key={item.LABEL} className={s.item}>{item.LABEL}: {item.REFERENCE}</li>
-        })}
-      </ul>
+      <img src={weatherData?.current.condition.icon} alt='ícone' />
+      {renderList()}
     </section>
   )
+
 }
